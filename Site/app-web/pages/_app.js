7e8325f8e089417fc9eed9274ed1ffa6937e5b0c
dikdableu <dikdableu@hotmail.fr>
2022-07-12 18:28:55 +0200

    importation de bootstrap pour le projet

--- Site/app-web/pages/_app.js
+++ Site/app-web/pages/_app.js
@@ -3,6 +3,8 @@ import "bootstrap/dist/css/bootstrap.css";
 import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+
+  // Importation de bootstrap pour le projet
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap");
   }, []);
