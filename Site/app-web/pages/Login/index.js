99bbba882f12c678cb4b47a43dbad54c25179c36
dikdableu <dikdableu@hotmail.fr>
2022-07-12 18:20:48 +0200

    Ajout front Login

---
+++ Site/app-web/pages/Login/index.js
@@ -0,0 +1,40 @@
+import React, {useState, useRef} from "react";
+import styles from "../../styles/Login.module.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+export default function Login() {
+
+  const inputLoginRef = useRef();
+  const inputPasswordRef = useRef();
+
+  return (
+    <div className={styles.containerParent}>
+      <Container className="h-100 w-100 " fluid>
+        <Row className="h-100 d-flex flex-column justify-content-center align-items-center">
+        <Row className="justify-content-center align-items-center">
+            <Row xs="auto" md="auto" lg="auto" className="justify-content-center align-items-center">
+              <p className={styles.title}>Connectez vous pour accéder au site </p>
+            </Row>
+          </Row>
+          <Row className="justify-content-center align-items-center">
+            <Row xs="auto" md="auto" lg="auto" className="justify-content-center align-items-center">
+              <input id="login" type="text" ref={inputLoginRef} placeholder="Login" className={styles.containerLogin} />
+            </Row>
+          </Row>
+          <Row className="justify-content-center align-items-center">
+            <Row xs="auto" md="auto" lg="auto" className="justify-content-center align-items-center">
+              <input id="password" type="password" ref={inputPasswordRef} placeholder="Password" className={styles.containerPassword}/>
+            </Row>
+          </Row>
+          <Row className="justify-content-center align-items-center">
+            <Row xs="auto" md="auto" lg="auto" className="justify-content-center align-items-center">
+              <button className={styles.buttonLogin} id="validate"> Validate </button>
+            </Row>
+          </Row>
+        </Row>
+      </Container>
+    </div>
+  );
+}
